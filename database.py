be91a4a14cc8935201f6916732c80c4f084d0d4c
Gueguelas <157625816+MiguelAraujoDeSouza@users.noreply.github.com>
2025-08-24 18:55:16 -0300

    docs(env): update envs in database conection

--- database.py
+++ database.py
@@ -8,10 +8,10 @@ load_dotenv()
 def conecta_primeiro():
     try:
         conn = psycopg2.connect(
-            user=os.getenv('user'),
-            password= os.getenv('password'),
-            host=os.getenv('host'),
-            port=os.getenv('port'),
+            user=os.getenv('user_primeiro'),
+            password= os.getenv('password_primeiro'),
+            host=os.getenv('host_primeiro'),
+            port=os.getenv('port_primeiro'),
             database=os.getenv('database_primeiro')
         )
         return conn
@@ -21,10 +21,10 @@ def conecta_primeiro():
 def conecta_segundo():
     try:
         conn = psycopg2.connect(
-            user=os.getenv('user'),
-            password= os.getenv('password'),
-            host=os.getenv('host'),
-            port=os.getenv('port'),
+            user=os.getenv('user_segundo'),
+            password= os.getenv('password_segundo'),
+            host=os.getenv('host_segundo'),
+            port=os.getenv('port_segundo'),
             database=os.getenv('database_segundo')
         )
         return conn
