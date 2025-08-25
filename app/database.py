03bc0aa1af5493fcbf9e18fc8c80ac20361182e1
Gueguelas <miguel0araujo0souza@gmail.com>
2025-08-24 22:29:27 -0300

    refactor(database): improve connection handling and error messages
    refactor(main): enhance readability by organizing import statements
    
    refactor(service): clean up code formatting and improve error handling
    
    build(dependencies): update requirements.txt to include black for code formatting

--- app/database.py
+++ app/database.py
@@ -5,31 +5,34 @@ from psycopg2 import Error
 
 load_dotenv()
 
+
 def conecta_primeiro():
     try:
         conn = psycopg2.connect(
-            user=os.getenv('user_primeiro'),
-            password= os.getenv('password_primeiro'),
-            host=os.getenv('host_primeiro'),
-            port=os.getenv('port_primeiro'),
-            database=os.getenv('database_primeiro')
+            user=os.getenv("user_primeiro"),
+            password=os.getenv("password_primeiro"),
+            host=os.getenv("host_primeiro"),
+            port=os.getenv("port_primeiro"),
+            database=os.getenv("database_primeiro"),
         )
         return conn
     except Error as e:
         print(f"Ocorreu um erro ao tentar conectar ao banco de dados do primeiro: {e}")
 
+
 def conecta_segundo():
     try:
         conn = psycopg2.connect(
-            user=os.getenv('user_segundo'),
-            password= os.getenv('password_segundo'),
-            host=os.getenv('host_segundo'),
-            port=os.getenv('port_segundo'),
-            database=os.getenv('database_segundo')
+            user=os.getenv("user_segundo"),
+            password=os.getenv("password_segundo"),
+            host=os.getenv("host_segundo"),
+            port=os.getenv("port_segundo"),
+            database=os.getenv("database_segundo"),
         )
         return conn
     except Error as e:
         print(f"Ocorreu um erro ao tentar conectar ao banco de dados do segundo: {e}")
+
 
 def encerra_conexao(conn):
     if conn:
--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
-from service import pegar_dados, pegar_tabelas, pegar_colunas, criar_tabela_temp, inserir_dados, chamar_procedure, pegar_colunas_tipo
+from service import (
+    pegar_dados,
+    pegar_tabelas,
+    pegar_colunas,
+    criar_tabela_temp,
+    inserir_dados,
+    chamar_procedure,
+    pegar_colunas_tipo,
+)
+
 
 def atualizar_banco():
-    lista_tabelas = pegar_tabelas('public')
-    string_tabelas = ', '.join(lista_tabelas)
-    print(f'Começando o procedimento nas tabelas: {string_tabelas}')
+    lista_tabelas = pegar_tabelas("public")
+    string_tabelas = ", ".join(lista_tabelas)
+    print(f"Começando o procedimento nas tabelas: {string_tabelas}")
     for tabela in lista_tabelas:
-        print(f'Tabela: {tabela}')
+        print(f"Tabela: {tabela}")
         # Pegando colunas da tabela
         colunas = pegar_colunas(tabela)
         # Criando tabela temp
@@ -14,6 +23,7 @@ def atualizar_banco():
         inserir_dados(pegar_dados(tabela, colunas), tabela, colunas)
         # Puxando procedure
         chamar_procedure(tabela)
-    print('Banco atualizado!')
-   
+    print("Banco atualizado!")
+
+
 atualizar_banco()
